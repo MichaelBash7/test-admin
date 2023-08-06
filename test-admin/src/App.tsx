fa9bad6b267155c81c8a16540e7c0d46e91248ae
bashmisha710 <bashmisha710@gmail.com>
2023-08-06 19:51:18 +0300

    Initial commit

---
+++ test-admin/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Admin, Resource} from 'react-admin';
+import { dataProvider } from './dataProvider';
+import {UserList} from "./users";
+import {PostCreate, PostEdit, PostList} from "./posts";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import {Dashboard} from "./Dashboard";
+import {authProvider} from "./authProvider";
+
+export const App = () => (
+    <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
+        <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name"/>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    </Admin>
+);
+
